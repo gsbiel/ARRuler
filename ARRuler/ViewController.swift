8f7c7ac9f6d4aa4cc98eae2814232403ce12a570
user161182 <gabriel.sgaspar@yahoo.com.br>
2020-02-04 06:43:27 -0500

    adicionando pontos nas superficies do espaco virtual que o usuario tocar

--- ARRuler/ViewController.swift
+++ ARRuler/ViewController.swift
@@ -43,4 +43,41 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touchLocation = touches.first?.location(in: sceneView){
+            
+            //.featurePoint detecta a localizacao de um ponto em uma superficie horizontal presente no Scene, se existir.
+            let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            if let hitResult = hitTestResults.first {
+                addDot(at: hitResult)
+            }
+        }
+    }
+    
+    private func addDot(at hitResult: ARHitTestResult) {
+        
+        // Criando uma esfera
+        let dotGeometry =  SCNSphere(radius: 0.005)
+        // Criando um material
+        let material = SCNMaterial()
+        // Definindo o conteudo do material com preenchimento vermelho
+        material.diffuse.contents = UIColor.red
+        // Inserindo o material na esfera
+        dotGeometry.materials = [material]
+        
+        // Criando um node onde a esfera vai ficar posicionada
+        let doteNode = SCNNode(geometry: dotGeometry)
+        
+        // Pegando as coordenadas do ponto no espaco virtual 3D em que o usuario tocou
+        let x_position = hitResult.worldTransform.columns.3.x
+        let y_position = hitResult.worldTransform.columns.3.y
+        let z_position = hitResult.worldTransform.columns.3.z
+        // Definindo a posicao do node no espaco 3D
+        doteNode.position = SCNVector3(x_position, y_position, z_position)
+        
+        // Inserindo o node no espaco 3D(Scene)
+        sceneView.scene.rootNode.addChildNode(doteNode)
+        
+    }
 }
